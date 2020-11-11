1a031a8b388693016a7870471298dc030a29e8e9
Mingshen Sun <bob@mssun.me>
2020-11-11 10:39:15 -0800

    Test GitHub Action

--- scripts/swiftlint.sh
+++ scripts/swiftlint.sh
@@ -1,6 +1,6 @@
 SWIFTLINT_VERSION="0.41.*"
 
-if [[ -f "${SRCROOT}/.ci-env" ]]; then
+if [[  "${CI}" == "true" ]]; then
   echo "Running in a Continuous Integration environment. Linting is skipped."
   return
 fi
